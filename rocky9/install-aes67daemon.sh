fc7bb4c58212162875efaf02881189e325bc3d27
Nikos Toutountzoglou <nikos.toutou@gmail.com>
2024-03-15 09:21:37 +0100

    Update install-aes67daemon.sh

--- rocky9/install-aes67daemon.sh
+++ rocky9/install-aes67daemon.sh
@@ -23,7 +23,7 @@ sudo dnf update
 
 # Install all dependencies
 echo "Installing all dependencies for building the AES67 Ravenna Daemon package."
-sudo dnf install psmisc clang git npm boost-devel valgrind alsa-lib alsa-lib-devel pulseaudio-libs-devel linuxptp systemd-devel avahi-devel kernel-headers-$(uname -r)
+sudo dnf install psmisc clang git npm boost-devel valgrind alsa-lib alsa-lib-devel pulseaudio-libs-devel linuxptp systemd-devel kernel-headers-$(uname -r)
 
 # Create a working source dir
 mkdir -p $PKGDIR
